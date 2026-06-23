8d2b8c0cf3225ea96bf0f00d6cb1f916cf8289b0
mohamed <m.yasser.dev@gmail.com>
2026-06-23 11:13:35 +0300

    cores

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-
 const express = require("express")
+import cors from "cors";
 
 const dns = require('dns');
 dns.setServers(["1.1.1.1", "1.0.0.1", "8.8.8.8"]);
@@ -8,6 +8,7 @@ require("dotenv").config();
 const main = require("./controllers/shop.controller")
 const router = require("./routes/route")
 const app = express()
+app.use(cors());
 app.use(express.json());
 const { MongoClient } = require('mongodb');
 const mongoose = require("mongoose");
